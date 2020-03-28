f7a49cba82ba016bae72432807804a9caa2765ab
Tong ZHAN <zhantong1994@163.com>
2020-03-28 15:42:44 +0800

    添加CommentCount到Article

---
+++ BearBlog/Plugins/Comment/DataModels/ArticleDataModel.cs
@@ -0,0 +1,10 @@
+﻿using BearBlog.Models;
+
+namespace BearBlog.Plugins.Article.DataModels
+{
+    public partial class ArticleDataModel
+    {
+        [Visibility(Visibility.Full)] public int? CommentCollectionId => _article.CommentCollectionId;
+        public int CommentCount => _article.CommentCollection.Comments.Count;
+    }
+}
